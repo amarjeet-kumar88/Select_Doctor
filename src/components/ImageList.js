ed506d6a404d6d03dd7dd4b357ec2753aaea7fa3
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2024-02-25 11:16:01 +0530

    added style

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,15 +4,15 @@ import ImageCard from "./ImageCard";
 
 const ImageList = ({ images }) => {
   return (
-    <div className="mt-[80px] ">
+       <div className="mt-[80px] ">
         <div class=" flex flex-col justify-center items-center mb-16">
-  <div class="p-1 font-semibold text-2xl ">Selector Doctor</div>
-  <div class="w-[80px] h-[4px] border-b border-blue-400 flex-grow"></div>
-</div>
+       <div class="p-1 font-semibold text-2xl ">Selector Doctor</div>
+        <div class="w-[80px] h-[4px] border-b border-blue-400 flex-grow"></div>
+     </div>
 
     <div className="container mx-auto mt-8">
         
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-3 gap-4 md:grid-cols-4">
         {images.map((image, index) => (
           <ImageCard key={index} image={image} />
         ))}
